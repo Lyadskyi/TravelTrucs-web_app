652002df68404c9242588ca65b1ab3c0dc65509c
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-11-14 01:41:09 +0200

    Added a second card to the component DetailsFeatures.jsx-create
    Update a classname={css.deskDetails}

--- src/components/DetailsFeatures/DetailsFeatures.jsx
+++ src/components/DetailsFeatures/DetailsFeatures.jsx
@@ -3,41 +3,46 @@ import css from "./DetailsFeatures.module.css";
 export default function DetailsFeatures() {
   return (
     <>
-      <div className={css.wrapper}>
+      <div className={css.wrapperDetails}>
         <div className={css.badgesContainer}>
           <div className={css.list}>
             <svg width="16" height="16">
               <use href="/images/icons/sprite.svg#icon-automatic"></use>
             </svg>
-            <p className={css.description}>Automatic</p>
+            <p className={css.descDetails}>Automatic</p>
           </div>
           <div className={css.list}>
             <svg width="16" height="16">
               <use href="/images/icons/sprite.svg#icon-AC"></use>
             </svg>
-            <p className={css.description}>AC</p>
+            <p className={css.descDetails}>AC</p>
           </div>
           <div className={css.list}>
             <svg width="16" height="16">
               <use href="/images/icons/sprite.svg#icon-petrol"></use>
             </svg>
-            <p className={css.description}>Petrol</p>
+            <p className={css.descDetails}>Petrol</p>
           </div>
           <div className={css.list}>
             <svg width="16" height="16">
               <use href="/images/icons/sprite.svg#icon-kitchen"></use>
             </svg>
-            <p className={css.description}>Kitchen</p>
+            <p className={css.descDetails}>Kitchen</p>
           </div>
           <div className={css.list}>
             <svg width="16" height="16">
               <use href="/images/icons/sprite.svg#icon-radio"></use>
             </svg>
-            <p className={css.description}>Radio</p>
+            <p className={css.descDetails}>Radio</p>
           </div>
         </div>
-
-        <h3 className={css.title}>Vehicle details</h3>
+        <h3 className={css.titleDetails}>Vehicle details</h3>
+      </div>
+      <div className={css.wrapperForm}>
+        <h3 className={css.titleForm}>Book your campervan now</h3>
+        <p className={css.descForm}>
+          Stay connected! We are always ready to help you.
+        </p>
       </div>
     </>
   );
