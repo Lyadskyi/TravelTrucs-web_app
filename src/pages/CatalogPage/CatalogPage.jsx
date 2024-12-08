9d7425e24eaee6ff66c7eee10be3df5e49642709
Dmytro Liadskyi <145874926+Lyadskyi@users.noreply.github.com>
2024-12-08 23:21:46 +0200

    Added radio buttons in the CatalogPage.jsx-create
    Updated a CatalogPage.module.css

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -32,9 +32,124 @@ export default function CatalogPage() {
         <h3 className={css.filters}>Filters</h3>
         <div className={css.equipment}>
           <h3 className={css.titleEquipment}>Vehicle equipment</h3>
+          <ul className={css.filtersContainer}>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">AC</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Automatic</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Bathroom</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Gas</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Kitchen</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Microwave</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Petrol</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Radio</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Refrigerator</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">TV</button>
+            </li>
+            <li className={css.filtersCheckBoxes}>
+              <label>
+                <input type="checkbox" name="terms" />
+              </label>
+              <button type="button">Water</button>
+            </li>
+          </ul>
         </div>
         <div className={css.type}>
           <h3 className={css.titleEquipment}>Vehicle type</h3>
+          <ul className={css.filtersContainer}>
+            <li>
+              <input name="form" type="radio" id="1" value="Van"></input>
+              <label className={css.filtersLabels} htmlFor="1">
+                <svg
+                  className={css.icon}
+                  color="currentColor"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <use href="/images/icons/sprite.svg#icon-grid_1x2"></use>
+                </svg>
+                Alcove
+              </label>
+            </li>
+            <li>
+              <input name="form" type="radio" id="2" value="Van"></input>
+              <label className={css.filtersLabels} htmlFor="2">
+                <svg
+                  className={css.icon}
+                  color="currentColor"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <use href="/images/icons/sprite.svg#icon-grid_2x2"></use>
+                </svg>
+                Fully Integrated
+              </label>
+            </li>
+            <li>
+              <input name="form" type="radio" id="3" value="Van"></input>
+              <label className={css.filtersLabels} htmlFor="3">
+                <svg
+                  className={css.icon}
+                  color="currentColor"
+                  width="16"
+                  height="16"
+                  aria-hidden="true"
+                >
+                  <use href="/images/icons/sprite.svg#icon-grid_3x3"></use>
+                </svg>
+                Van
+              </label>
+            </li>
+          </ul>
         </div>
         <button className={css.btn}>Search</button>
       </div>
